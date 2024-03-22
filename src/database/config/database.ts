a5a5661128353bc66749778f3b66fc1f0a1b443d
Marcos Santana <marcosshotokan10@gmail.com>
2024-03-22 17:05:47 -0300

    feat : config sequelize

---
+++ src/database/config/database.ts
@@ -0,0 +1,10 @@
+import { Options } from "sequelize";
+const config: Options = {
+  "username": "root",
+  "password": '',
+  "database": "loja",
+  "host": "127.0.0.1",
+  "dialect": "mysql"
+}
+
+export = config;
